91f2a23a18ebdbe8cfb1d951b7a490576f69c44c
Juan Pulgar <jpulgarg@ripley.com>
2022-11-02 01:19:43 -0300

    correcion mongoDb

---
+++ src/main/java/com/meli/desafio/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.meli.desafio.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Value("${spring.data.mongodb.uri}")
+	private String uriDb;
+	
+	@Value("${spring.data.mongodb.database}")
+	private String dbName;
+	
+	public @Bean MongoClient mongoClient() {
+		return MongoClients.create(uriDb);
+	}
+
+	public @Bean
+	MongoTemplate mongoTemplate() {
+		return new MongoTemplate(mongoClient(), dbName);
+	}
+
+}
